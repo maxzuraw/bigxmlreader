5d45c98ec54349539ce05881c4498f95d84b2990
Max Zurawski <maksymilian.zurawski@consileon.pl>
2025-01-16 13:48:10 +0100

    properties adjustments

--- src/main/java/pl/bigxml/reader/config/XmlReaderConfig.java
+++ src/main/java/pl/bigxml/reader/config/XmlReaderConfig.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Data
 public class XmlReaderConfig {
 
-    private boolean isNamespaceAware;
+    private boolean namespaceAware;
     private boolean removeNamespaceManuallyEnabled;
     private String namespacesCommaSeparated;
 
